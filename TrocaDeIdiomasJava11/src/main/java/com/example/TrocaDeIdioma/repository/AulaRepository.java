043cdf16256f2952817445e04f5cec36ad8b0e7d
LojaInterativa-Gabriel-Moraes <gabriel.moraes@lojainterativa.com>
2023-02-27 02:53:44 -0300

    projeto att

--- TrocaDeIdiomasJava11/src/main/java/com/example/TrocaDeIdioma/repository/AulaRepository.java
+++ TrocaDeIdiomasJava11/src/main/java/com/example/TrocaDeIdioma/repository/AulaRepository.java
@@ -2,8 +2,10 @@ package com.example.TrocaDeIdioma.repository;
 
 import com.example.TrocaDeIdioma.model.Aula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
 public interface AulaRepository extends JpaRepository<Aula, Long> {
 
 }
--- TrocaDeIdiomasJava11/src/main/java/com/example/TrocaDeIdioma/repository/SolicitacaoAulaRepository.java
+++ TrocaDeIdiomasJava11/src/main/java/com/example/TrocaDeIdioma/repository/SolicitacaoAulaRepository.java
@@ -3,7 +3,9 @@ package com.example.TrocaDeIdioma.repository;
 
 import com.example.TrocaDeIdioma.model.SolicitacaoAula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SolicitacaoAulaRepository extends JpaRepository<SolicitacaoAula, Long> {
 
 }
